484733b8b786cd1d35ce1f0967a09335972ff701
bobbyhadz <opmftw@gmail.com>
2024-04-06 13:50:37 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
 
-function App() {
+import {useState} from 'react';
+
+const App = () => {
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    console.log('form submitted ✅');
+  };
+
   return (
     <div>
-      <h2>bobbyhadz.com</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="first"
+          name="first"
+          value={first}
+          onChange={event => setFirst(event.target.value)}
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          id="last"
+          name="last"
+          value={last}
+          onChange={event => setLast(event.target.value)}
+          autoComplete="off"
+        />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
